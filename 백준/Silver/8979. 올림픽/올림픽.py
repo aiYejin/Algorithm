2de80bfbcc6709f97c68afa8e02290475368ed35
Yejin <114991029+aiYejin@users.noreply.github.com>
2024-01-14 23:02:27 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,10 +3,12 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 arr = list(list(map(int, input().split())) for _ in range(N))
-k = arr[K-1]
-arr.sort(key = lambda x: (x[0], x[1], x[2]), reverse = True)
+for a in arr:
+    if a[0] == K:
+        k = [a[1],a[2],a[3]]
+arr.sort(key = lambda x: (-x[1], -x[2], -x[3]))
 
 for i in range(N):
-    if arr[i] == k:
+    if [arr[i][1], arr[i][2], arr[i][3]] == k:
         print(i+1)
         break
