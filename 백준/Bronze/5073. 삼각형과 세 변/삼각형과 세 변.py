802528aefd179216eacabd93a56f96d7bfb1efad
Yejin <114991029+aiYejin@users.noreply.github.com>
2024-01-30 23:59:55 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -1,16 +1,13 @@
-import sys
-input = sys.stdin.readline
-
-while(1):
+while True:
     arr = list(map(int, input().split()))
-    if arr[0] == 0 and arr[1] == 0 and arr[2] == 0:
+    if [0,0,0] == arr:
         break
     arr.sort()
-    if arr[0]==arr[1] and arr[1]==arr[2]:
+    if arr[0] == arr[2]:
         print("Equilateral")
-    elif arr[2] >= arr[1]+arr[0]:
+    elif arr[0] + arr[1] <= arr[2]:
         print("Invalid")
-    elif (arr[0]==arr[1]) or (arr[1]==arr[2]):
+    elif arr[0] == arr[1] or arr[1] == arr[2]:
         print("Isosceles")
     else:
         print("Scalene")
